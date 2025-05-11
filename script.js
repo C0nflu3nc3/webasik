603098cd4a330d958eed29eace46e64c8e0cda8f
C0nflu3nc3 <133865428+C0nflu3nc3@users.noreply.github.com>
2025-05-11 20:37:05 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const base = 100;
+    document.getElementById('calc').onclick = () => {
+      let price = base;
+      price *= +document.getElementById('edu').value;
+      price *= +document.getElementById('net').value;
+      price += +document.getElementById('caste').value;
+
+      document.querySelectorAll('.skill:checked').forEach(el => price += +el.value);
+      document.querySelectorAll('input[name="age"]').forEach(r => { if (r.checked) price *= +r.value });
+
+      document.querySelectorAll('.rep').forEach(el => {
+        if (!el.checked) return;
+        const c = el.dataset.coef;
+        if (c) price *= +c;
+        else price -= 20;
+      });
+
+      document.getElementById('result').textContent = `Price: $${price.toFixed(2)}`;
+    }
